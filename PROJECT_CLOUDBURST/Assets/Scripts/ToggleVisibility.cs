5022a6d80d78acf0640a5779b1e3c7f5a052ba78
cbrown3 <cbrow2014@gmail.com>
2016-09-25 18:10:26 -0400

    assets

---
+++ PROJECT_CLOUDBURST/Assets/Scripts/ToggleVisibility.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleVisibility : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float pause = Input.GetAxisRaw("Pause");
+        if (pause > 0)
+        {
+            // toggle visibility:
+            if(gameObject.activeSelf == false)
+            {
+                gameObject.SetActive(true);
+            }
+            else if (gameObject.activeSelf == true)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
